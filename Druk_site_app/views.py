387c941be3b96a2a56c2b7fa3c65f821fc005f83
SteepBoss <aspirenumeric@gmail.com>
2023-04-07 18:02:20 +0300

    Добавлена кнопка входа а так же перенаправление на нее , создан login.urls

--- Druk_site_app/views.py
+++ Druk_site_app/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'Druk_site_app/index.html')
 
-
-def registration(request):
-    return render(request, 'Druk_site_app/registration.html')
 def write_us(request):
     return render(request, 'Druk_site_app/write_us.html')
 
