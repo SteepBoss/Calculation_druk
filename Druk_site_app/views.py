8e0d2b63370658db2e4f71536bf9582e1492a1bd
SteepBoss <aspirenumeric@gmail.com>
2023-04-08 21:58:58 +0300

    создана кнопка регистрации,обновлен вход

--- Druk_site_app/views.py
+++ Druk_site_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'Druk_site_app/index.html')
 
 def write_us(request):
     return render(request, 'Druk_site_app/write_us.html')
+
 
 
